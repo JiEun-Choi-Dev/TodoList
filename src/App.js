fd00e6aa7bcb7c3d491c6e7c5730cfb8b612bb5e
jieunchoi <jehy0807@gmail.com>
2020-03-16 11:28:13 +0900

    오류수정완료 (todos props 누락)

--- src/App.js
+++ src/App.js
@@ -8,28 +8,76 @@ class App extends Component {
 
 id = 3
 
-state ={
+state = {
   input: '',
   todos: [
-    { id:0, text: '리액트 소개', checked: false },
-    { id:1, text: '리액트 소개', checked: true },
-    { id:2, text: '리액트 소개', checked: false }
+    { id: 0, text: '리액트 소개', checked: false },
+    { id: 1, text: '리액트 소개', checked: true },
+    { id: 2, text: '리액트 소개', checked: false }
   ]
-}
+};
 
-handleChange = (e) =>{
+handleChange = (e) => {
   this.setState({
     input: e.target.value
   });
 }
 
 handleCreate = () => {
-  const { input, todos}
+  const { input, todos } = this.state;
+  this.setState({
+    input:'',
+    todos : todos.concat({
+      id: this.id++,
+      text: input,
+      checked: false
+    })
+  });
 }
+
+handleKeyPress = (e) => {
+  if(e.Key === 'Enter') {
+    this.handleCreate();
+  }
+}
+
+handleToggle = (id) => {
+  const { todos } = this.state;
+
+  const index = todos.findIndex(todo => todo.id === id);
+  const selected = todos[index]; 
+
+  const nextTodos = [...todos]; 
+
+
+  nextTodos[index] = { 
+    ...selected, 
+    checked: !selected.checked
+  };
+
+  this.setState({
+    todos: nextTodos
+  });
+}
+
 render(){
+  const { input, todos } = this.state;
+  const {
+    handleChange,
+    handleCreate,
+    handleKeyPress,
+    handleToggle
+  } = this;
+
   return (
-    <TodoListTemplate form={<Form/>}>
-      <TodoItemList/>
+    <TodoListTemplate form={(<Form
+    value={input}
+    onKeyPress={handleKeyPress}
+    onChange={handleChange}
+    onCreate={handleCreate}
+    />
+    )}>
+      <TodoItemList todos={todos} onToggle={handleToggle}/>
     </TodoListTemplate>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.checked !== nextProps.checked;
+  }
     render() {
       const { text, checked, id, onToggle, onRemove } = this.props;
   
       return (
         <div className="todo-item" onClick={() => onToggle(id)}>
           <div className="remove" onClick={(e) => {
-            e.stopPropagation(); // onToggle 이 실행되지 않도록 함
+            e.stopPropagation();
             onRemove(id)}
           }>&times;</div>
           <div className={`todo-text ${checked && 'checked'}`}>
